36383bce770c38cc975fc70d5eb1fd0aa4cf4886
szymonkenig81-glitch <szymonkenig81@gmail.com>
2026-07-11 23:30:55 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5174,
+    host: true,
+  },
 })
